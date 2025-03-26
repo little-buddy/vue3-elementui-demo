1fef8608e27f08333e332636b14caae9659a6f97
buddy <litoubuddy@gmail.com>
2025-03-26 21:08:17 +0800

    产品信息字段确定

--- src/pages/index/basicData/productInfo.vue
+++ src/pages/index/basicData/productInfo.vue
@@ -1,6 +1,12 @@
+<script lang="ts" setup>
+const data = [
+
+  { prdId: '1',     prdProperty: '零件', prdType: '固定资产', prdName: '11', brand: '11', modeType: '111', prdUnit: '盒', rep: '办公品仓库', repId: '0001007-772f1650f45e417b859828e32f4d4f5d' },
+]
+</script>
+
 <template>
   <div>
     产品信息
   </div>
 </template>
-
